7ae9bf47d1f493b95e18c70504745f42c6cda5bc
Cd Chen <cdchen@cdchen.idv.tw>
2012-10-13 02:30:14 +0800

    Initial import.

---
+++ src/nicedjango/db/fields/__init__.py
@@ -0,0 +1,23 @@
+
+from django.db.models.fields import CharField
+from django_extensions.db.fields import UUIDField
+import shortuuid
+
+
+class ShortUUIDField(UUIDField):
+    '''
+    用來產生適合 HTTP 使用的 UUID 欄位。
+    '''
+
+    def pre_save(self, model_instance, add):
+        value = CharField.pre_save(self, model_instance, add)
+        if self.auto and add and value is None:
+            value = shortuuid.encode(self.create_uuid())
+            setattr(model_instance, self.attname, value)
+        else:
+            if self.auto and not value:
+                value = shortuuid.encode(self.create_uuid())
+                setattr(model_instance, self.attname, value)
+        return value
+
+# End of file.
